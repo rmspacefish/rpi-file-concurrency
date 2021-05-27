3ab062805eae74da7f4e09f6ddefee360dc21153
Robin Mueller <robin.mueller.m@gmail.com>
2021-05-28 00:39:50 +0200

    adding threads

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <thread>
+
+void readerFunc();
+void writerFunc();
 
 int main() {
     std::cout << "Hello World!" << std::endl;
+
+    std::thread readerTask(readerFunc);
+    std::thread writerTask(writerFunc);
+    readerTask.join();
+    writerTask.join();
+
+	std::cout << "Finished." << std::endl;
+}
+
+void readerFunc() {
+
+}
+
+void writerFunc() {
+
 }
